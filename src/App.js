00cfafe179d7f48513c22cf5b4a7e24bb987dcd8
ann40a <ann40a@yandex.ru>
2019-07-26 20:03:01 +0300

    chore: configure application to use react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>About Me</h1>
+      <h2>Anna Sorokina, Full stack web developer</h2>
+    </>
+  );
+}
+
+export default App;
