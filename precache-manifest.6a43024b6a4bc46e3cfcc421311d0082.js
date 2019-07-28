40f47e52e8846b49203486d1995b870402c77c3e
ann40a <ann40a@yandex.ru>
2019-07-28 03:19:58 +0300

    release: deployment build v0.1.0

---
+++ precache-manifest.6a43024b6a4bc46e3cfcc421311d0082.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7eb4822dc0ba2ecfe2479a76433080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "344ea50fd5c6a66b4aec",
+    "url": "/static/css/main.791c1b25.chunk.css"
+  },
+  {
+    "revision": "fa2eeab577da614267e6",
+    "url": "/static/js/2.d155ab5a.chunk.js"
+  },
+  {
+    "revision": "344ea50fd5c6a66b4aec",
+    "url": "/static/js/main.05b10878.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "462e444a1d567e3ee34b2e815a1ed880",
+    "url": "/static/media/lorem-ipsum.462e444a.png"
+  },
+  {
+    "revision": "ecd095eaeb39ce0607cd4ba62342fb94",
+    "url": "/static/media/web-app-vulnerabilities.ecd095ea.png"
+  }
+]);
